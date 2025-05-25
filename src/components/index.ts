c7564c867d277023fad563295ab8c73dda06ed70
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-05-25 22:03:53 +0700

    feat: add Modal component with supporting subcomponents and context

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { Avatar, type AvatarProps } from './avatar';
 export { Button, IconButton, type ButtonProps, type IconButtonProps } from './button';
 export { Drawer, type DrawerProps } from './drawer';
+export { Modal, type ModalProps } from './modal';
 export { Popover, type PopoverProps } from './popover';
 export { TopNav, type TopNavProps } from './top-nav';
 export { UserMenuPanel, type UserMenuPanelProps } from './top-nav/user-menu-panel';
