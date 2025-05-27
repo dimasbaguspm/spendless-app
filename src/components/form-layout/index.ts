ccf824fc6410e5856393e76e5eb8913a5002632b
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-05-27 21:36:16 +0700

    feat: replace FormLayoutSection with FormLayoutTitle in FormLayout composition

--- src/components/form-layout/index.ts
+++ src/components/form-layout/index.ts
@@ -1,19 +1,19 @@
 import { FormLayout as BaseFormLayout } from './form-layout';
 import { FormLayoutField } from './form-layout-field';
-import { FormLayoutSection } from './form-layout-section';
+import { FormLayoutTitle } from './form-layout-title';
 
 type FormLayoutCompositionModel = {
   Field: typeof FormLayoutField;
-  Section: typeof FormLayoutSection;
+  Title: typeof FormLayoutTitle;
 };
 
 const FormLayoutComposition = {
   Field: FormLayoutField,
-  Section: FormLayoutSection,
+  Title: FormLayoutTitle,
 } satisfies FormLayoutCompositionModel;
 
 export const FormLayout = Object.assign(BaseFormLayout, FormLayoutComposition);
 
 export type { FormLayoutProps } from './form-layout';
 export type { FormLayoutFieldProps } from './form-layout-field';
-export type { FormLayoutSectionProps } from './form-layout-section';
+export type { FormLayoutTitleProps } from './form-layout-title';
